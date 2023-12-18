7e9f8e5d8157fa4a56c6b13ab4be014a34617bfa
Burl <burl@burlrice.com>
2023-12-18 14:57:28 -0600

    onConnect stub out

--- Dialogs/QSearch.py
+++ Dialogs/QSearch.py
@@ -7,14 +7,16 @@ from tkinter import SE
 
 from PyQt5 import QtCore
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QAbstractItemView, QHeaderView, QLabel, QPushButton, QTableWidget
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import QTimer, QSize
+from PyQt5.QtCore import QTimer, QSize, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QIcon, QMovie, QPixmap
 
 from .path import resolveUi, resolveImage
 
 class QSearch(QtWidgets.QDialog):
+    printerDetected = pyqtSignal(str, str)
+    
     def __init__(self):
         super().__init__()
         loadUi(resolveUi('search.ui'), self)
@@ -24,17 +26,27 @@ class QSearch(QtWidgets.QDialog):
         self.printers = {}
         self.onRefresh()
         
+        self.table = self.findChildren(QTableWidget, 'tableWidget')[0];
+        self.table.setColumnCount(3)
+        self.table.setHorizontalHeaderLabels(['Address', 'Name', ''])
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.verticalHeader().setVisible(False)
+        self.table.setShowGrid(False)
+        self.table.setSelectionMode(QAbstractItemView.SingleSelection)
+        
+        self.printerDetected.connect(self.addPrinter)
+
     def onRefresh(self):
         self.refresh.setEnabled(False)
 
-        self.icon = self.refresh.icon()
+        self.refreshIcon = self.refresh.icon()
 
         self.movie = QMovie(resolveImage('Refresh.gif'))
         self.movie.frameChanged.connect(self.onFrameChanged)
         self.movie.start();
         
         self.timer = QTimer()
-        self.timer.timeout.connect(self.stop_animation)
+        self.timer.timeout.connect(self.onRefreshComplete)
         self.timer.start(2000)
         self.thread = threading.Thread(target=self.broadcast)
         self.thread.start()        
@@ -51,13 +63,13 @@ class QSearch(QtWidgets.QDialog):
             readable, _, _ = select.select([udp], [], [], .250) 
             for sock in readable:
                 data, address = udp.recvfrom(1024)
-                self.printers[address[0]] = data.decode()
-                print(self.printers)
+                self.printerDetected.emit(address[0], data.decode())
+                self.printerDetected.emit(f'{address[0]} x', data.decode())
                 
         udp.close()
         
-    def stop_animation(self):
-        self.refresh.setIcon(self.icon)
+    def onRefreshComplete(self):
+        self.refresh.setIcon(self.refreshIcon)
         self.timer.stop()
         self.movie.stop()
         self.refresh.setEnabled(True)
@@ -65,3 +77,32 @@ class QSearch(QtWidgets.QDialog):
     def onFrameChanged(self):
         self.refresh.setIcon(QIcon(self.movie.currentPixmap()))
 
+    @pyqtSlot(str, str)
+    def addPrinter(self, addr, data):
+        self.printers[addr] = data
+
+        index = len(self.printers) - 1
+        
+        for row in range(self.table.rowCount()):
+            item = self.table.item(row, 0)
+            if item != None and item.text() == addr:
+                index = row
+                break
+        
+        self.table.setRowCount(len(self.printers))
+        
+        name = re.search(r'Head Name=([^,]+)', data)
+        
+        self.table.setCellWidget(index, 0, QLabel(addr))
+        self.table.setCellWidget(index, 1, QLabel(name.group(1) if name else data))
+        connect = QPushButton()
+        icon = QIcon(resolveImage('connect.png'))
+        connect.setIcon(icon)
+        connect.setIconSize(QSize(24, 24))
+        connect.clicked.connect(self.onConnect)
+        self.table.setCellWidget(index, 2, connect)
+
+    @pyqtSlot()
+    def onConnect(self):
+        for i in self.table.selectedIndexes():
+            print(i.row(), self.table.cellWidget(i.row(), 0).text())
